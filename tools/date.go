0174afbb1df20bdd262b425512779eacf706d827
xdm67x <m-ozkan@outlook.fr>
2026-04-21 23:12:41 +0200

    feat: add example hello and date tools

---
+++ tools/date.go
@@ -0,0 +1,28 @@
+package tools
+
+import (
+	"simple-agent/agent"
+	"time"
+)
+
+type DateTool struct{}
+
+func (t *DateTool) Name() string {
+	return "get_current_date"
+}
+
+func (t *DateTool) Description() string {
+	return "Returns the current date and time"
+}
+
+func (t *DateTool) Parameters() agent.ToolParams {
+	return agent.ToolParams{
+		Type:       "object",
+		Properties: map[string]any{},
+		Required:   []string{},
+	}
+}
+
+func (t *DateTool) Execute(args map[string]any) string {
+	return time.Now().String()
+}
